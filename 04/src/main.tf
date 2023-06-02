c38e3329886c7209c4c2220806ab39a04cfe563b
Fedor Metsger <fedor.metsger@gmail.com>
2023-06-02 17:10:44 +0300

    Add task 1 and 2 solution

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -1,3 +1,4 @@
+/*
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
@@ -7,4 +8,36 @@ resource "yandex_vpc_subnet" "develop" {
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
 }
+*/
+module "vpc" {
+  source    = "./vpc"
+  env_name  = "develop"
+  zone      = "ru-central1-a"
+  cidr      = "10.0.1.0/24"
+}
 
+module "test-vm" {
+  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name        = "develop"
+#  network_id      = yandex_vpc_network.develop.id
+  network_id      = module.vpc.vpc_id
+  subnet_zones    = ["ru-central1-a"]
+  subnet_ids      = [ module.vpc.subnet_id ]
+  instance_name   = "web"
+  instance_count  = 1
+  image_family    = "ubuntu-2004-lts"
+  public_ip       = true
+  
+  metadata = {
+    user-data          = data.template_file.userdata.rendered
+    serial-port-enable = 1
+  }
+}
+
+data template_file "userdata" {
+  template = file("${path.module}/cloud-init.yml")
+
+  vars = {
+    ssh_public_key = file("~/.ssh/id_rsa.pub")
+  }
+}
