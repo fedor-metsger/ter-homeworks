45b38e34075cf5a6598e14eff79b1ee0ea9bec1d
Fedor Metsger <fedor.metsger@gmail.com>
2023-06-03 22:04:07 +0300

    Add variables validation

--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -19,3 +19,29 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
+variable "ipv4_addr" {
+  type    = string
+  default = "192.168.0.1"
+#  default = "1920.1680.0.1"
+  description = "ip-адрес"
+
+  validation {
+    condition     = can(cidrhost(join("/", [var.ipv4_addr, 32]), 0))
+    error_message = "Invalid IPv4 address."
+  }
+}
+
+variable "ipv4_addr_list" {
+  type    = list(string)
+#  default = ["192.168.0.1", "1.1.1.1", "127.0.0.1"]
+  default = ["192.168.0.1", "1.1.1.1", "1270.0.0.1"]
+  description = "список ip-адресов"
+
+  validation {
+    condition = alltrue([
+      for a in var.ipv4_addr_list : can(cidrhost(join("/", [a, 32]), 0))
+    ])
+    error_message = "All elements must be valid IPv4 addresses."
+  }
+}
