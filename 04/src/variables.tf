d28f1044f00092ec0777517f710f85e8804cdb7d
Fedor Metsger <fedor.metsger@gmail.com>
2023-06-03 22:04:56 +0300

    Add variables validation

--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -53,5 +53,28 @@ variable "vm_db_name" {
   description = "example vm_db_ prefix"
 }
 
+variable "ipv4_addr" {
+  type    = string
+  default = "192.168.0.1"
+#  default = "1920.1680.0.1"
+  description = "ip-адрес"
 
+  validation {
+    condition     = can(cidrhost(join("/", [var.ipv4_addr, 32]), 0))
+    error_message = "Invalid IPv4 address."
+  }
+}
 
+variable "ipv4_addr_list" {
+  type    = list(string)
+#  default = ["192.168.0.1", "1.1.1.1", "127.0.0.1"]
+  default = ["192.168.0.1", "1.1.1.1", "1270.0.0.1"]
+  description = "список ip-адресов"
+
+  validation {
+    condition = alltrue([
+      for a in var.ipv4_addr_list : can(cidrhost(join("/", [a, 32]), 0))
+    ])
+    error_message = "All elements must be valid IPv4 addresses."
+  }
+}
