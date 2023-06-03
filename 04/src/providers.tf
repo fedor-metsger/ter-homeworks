6f88dad93a4c1cbec2096a23d473a680f09a3616
Fedor Metsger <fedor.metsger@gmail.com>
2023-06-03 19:56:04 +0300

    Add task #2 solution

--- 04/src/providers.tf
+++ 04/src/providers.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     yandex = {
@@ -5,7 +6,20 @@ terraform {
     }
   }
   required_version = ">=0.13"
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "bucket-for-terraform"
+    region     = "ru-central1"
+    key        = "terraform-tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1ge8m51046pv654acbv/etnoj5a7tvqiqh4j1bnj"
+    dynamodb_table = "tfstate-lock"
+  }
 }
+
 
 provider "yandex" {
   token     = var.token
@@ -13,3 +27,4 @@ provider "yandex" {
   folder_id = var.folder_id
   zone      = var.default_zone
 }
+
